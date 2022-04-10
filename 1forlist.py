0102462571ffea934ae021ae16d4c05cf5d5c8f0
hitfast123 <2020215114@stu.cqupt.edu.cn>
2022-04-10 22:37:23 +0800

    try to submit

--- 1forlist.py
+++ 1forlist.py
@@ -1,8 +1,15 @@
+from more_itertools import first
+
+
 for i in range(1):
     print ("now i can use git")
     print("i will try to make a list")
 
-lst=list(['i','love','learn','python'])
-for a in lst:
+first=list(['i','love','learn','python'])
+for a in first:
     print(a)
 #i make a list, and put some things in it
+
+second=list(["i","want","play","game"])
+for b in second:
+    print(b)
